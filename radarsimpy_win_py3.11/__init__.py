7e37fd7c0ad1e3cb0833a20bf8173fc01b033a83
Nicholas McCormick <91856410+Nick-Mc@users.noreply.github.com>
2024-03-18 16:38:54 -0500

    Add files via upload

---
+++ radarsimpy_win_py3.11/__init__.py
@@ -0,0 +1,33 @@
+"""
+A Radar Simulator for Python
+
+RadarSimPy is a powerful and versatile Python-based Radar Simulator that models
+radar transceivers and simulates baseband data from point targets and 3D models.
+Its signal processing tools offer range/Doppler processing, direction of arrival
+estimation, and beamforming using various cutting-edge techniques, and you can
+even characterize radar detection using Swerling's models. Whether you're a beginner
+or an advanced user, RadarSimPy is the perfect tool for anyone looking to develop
+new radar technologies or expand their knowledge of radar systems.
+
+---
+
+- Copyright (C) 2018 - PRESENT  radarsimx.com
+- E-mail: info@radarsimx.com
+- Website: https://radarsimx.com
+
+::
+
+    ██████╗  █████╗ ██████╗  █████╗ ██████╗ ███████╗██╗███╗   ███╗██╗  ██╗
+    ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██║████╗ ████║╚██╗██╔╝
+    ██████╔╝███████║██║  ██║███████║██████╔╝███████╗██║██╔████╔██║ ╚███╔╝ 
+    ██╔══██╗██╔══██║██║  ██║██╔══██║██╔══██╗╚════██║██║██║╚██╔╝██║ ██╔██╗ 
+    ██║  ██║██║  ██║██████╔╝██║  ██║██║  ██║███████║██║██║ ╚═╝ ██║██╔╝ ██╗
+    ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝     ╚═╝╚═╝  ╚═╝
+
+"""
+
+from .radar import Radar  # pylint: disable=no-name-in-module
+from .transmitter import Transmitter
+from .receiver import Receiver
+
+__version__ = "12.1.0"
