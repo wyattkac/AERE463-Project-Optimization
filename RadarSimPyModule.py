667c8f0f30a6cebf7ce87094f4c85b6614a8c9d4
[Nicholas Mccormick] <nicholas.mccormick11@gmail.com>
2024-04-16 16:18:46 -0500

    added ipynb to make looking at variables easier

--- RadarSimPyModule.py
+++ RadarSimPyModule.py
@@ -14,7 +14,7 @@ import numpy as np
 
 target = {
 
-        "model": 'C:/Users/Nicholas/Documents/ISU Spring 2024/AERE 463/AERE463-Project-Optimization/Example_Mesh1.stl',
+        "model": 'Example_Mesh1.stl',
         "location": (0, 0, 0),
   }
 
--- testFunction.py
+++ testFunction.py
@@ -3,7 +3,7 @@
 from RadarSimPyFunction import RadarSimPy_Function
 
 target = {
-    "model": 'C:/Users/Nicholas/Documents/ISU Spring 2024/AERE 463/AERE463-Project-Optimization/Example_Mesh1.stl',
+    "model": 'C:/Users/Nicholas/Documents/ISU Spring 2024/AERE 463/AERE463-Project-Optimization/test_vehicle_mesh.stl',
     "location": (0, 0, 0),
 }
 
